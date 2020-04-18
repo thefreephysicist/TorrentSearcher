d5e973f2dac46119c413235114bd684156749fab
Billy <bsmaat@gmail.com>
2020-04-18 20:51:37 +0100

    Added table for display and ID

---
+++ ThePirateBay/PirateBayModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ThePirateBay
+{
+	public class PirateBayModel
+	{
+		public PirateBayModel()
+		{
+		}
+
+		/// <summary>
+		/// Gets or sets the <see cref="PirateItemCollection"/>
+		/// </summary>
+		public PirateItemCollection PirateItems
+		{
+			get;
+			set;
+		}
+
+		public Query Query
+		{
+			get;
+			set;
+		}
+	}
+}
