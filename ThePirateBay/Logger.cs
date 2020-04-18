d3bce89ce2e8b871c8805a5011b12352b7b54920
Billy <bsmaat@gmail.com>
2020-04-18 20:16:52 +0100

    Added logger

---
+++ ThePirateBay/Logger.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ThePirateBay
+{
+	class Logger
+	{
+		static Logger()
+		{
+			Logs = new List<string>();
+		}
+
+		private Logger()
+		{
+		}
+
+		/// <summary>
+		/// Adds a log to the logger
+		/// </summary>
+		/// <param name="s">the log string</param>
+		public static void Log(string s)
+		{
+			Logs.Add(s);
+		}
+
+		private static IList<string> Logs
+		{
+			get;
+			set;
+		}
+	}
+}
