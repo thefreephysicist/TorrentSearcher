e914bffd94842b48c92bba1eba8fa5bb80ee416b
Billy <bsmaat@gmail.com>
2020-04-18 20:18:53 +0100

    Added query class

---
+++ ThePirateBay/Query.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ThePirateBay
+{
+	public class Query
+	{
+		public Query()
+		{
+		}
+
+		/// <summary>
+		/// The search term
+		/// </summary>
+		public string SearchTerm
+		{
+			get;
+			set;
+		}
+	}
+}
